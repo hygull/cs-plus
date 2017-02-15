bdbc59bfc9a81e8c0e32a0a15449a52e7c1e3b9a
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2017-02-15 16:38:34 +0530

    2 added a hello world source file

---
+++ sources/examples/hello.cs
@@ -0,0 +1,20 @@
+/*
+	Coded on : 15/02/2017
+	Aim      : To print hello world message onto the console.
+	Coded by : Rishikesh Agrawani.
+*/
+
+using System;
+namespace WelcomeApplication
+{
+	class Hello
+	{
+		public static void Main(String[] cmdArgs)
+		{
+			Console.WriteLine("Hello C# programmers!!!");
+			Console.Write("Is it true that ");
+			Console.Write("C# is a part of the .NET framework.");
+			Console.WriteLine();
+		}
+	}
+}
